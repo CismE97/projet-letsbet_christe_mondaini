6e30fdab38cfc0884da162ba594da196763ce04a
Christe Thibaud <thibaudchriste1997@gmail.com>
2017-11-29 09:36:43 +0100

    Installation de bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import 'bootstrap.native';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
