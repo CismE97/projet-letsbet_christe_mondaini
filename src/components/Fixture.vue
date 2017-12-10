df14b52951b5b040e1aaa769a81c2068b919928b
Alessandro Mondaini <mondaini.alessandro@gmail.com>
2017-12-10 19:59:39 +0100

    Ajout de la condition si le match est pariable on non + Affichage de la date dans un format "JJ/MM/AAAA hh:mm"

--- src/components/Fixture.vue
+++ src/components/Fixture.vue
@@ -6,15 +6,16 @@
             <p class="text-left">{{value.homeTeamName}} VS {{value.awayTeamName}}</p>
         </div>
         <div class="col-md-4 align-self-center text-left">
-            <p class="text-left">{{value.date}}</p>
+            <p class="text-left">{{ value.date | dateFormat }}</p>
         </div>
+        
         <div class="col-md-4">
-          <div v-if="value.result" class="row">
+          <div v-if="value.status != 'SCHEDULED'" class="row">
             <div class="col-md-12 col-12">
                 <p>{{value.result.goalsHomeTeam}} - {{value.result.goalsAwayTeam}}</p>
             </div>
           </div>
-          <div v-else class="row ">
+          <div v-else class="row">
             <form class="form-inline">
               <div class="col-md-8 col-12">
                 <input type="number" class="text-center" id="scoreA" placeholder="0">:
@@ -31,11 +32,22 @@
   </div>
 </template>
 <script>
+import moment from 'moment';
 export default {
-    props: ['value', 'index']
+    data() {
+        return {
+        };
+    },
+    props: ['value', 'index'],
+    filters: {
+        dateFormat: function (value) {
+            if (value) {
+                return moment(String(value)).format('DD/MM/YYYY hh:mm a');
+            }
+        }
+    }
 };
 </script>
-
 <style>
     form{
         margin:0;
